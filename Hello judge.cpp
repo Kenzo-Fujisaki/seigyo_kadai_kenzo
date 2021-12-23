1d10df1329e1b518dbabf1329d87050117fc9193
Kenzo-Fujisaki <85381022+Kenzo-Fujisaki@users.noreply.github.com>
2021-12-23 18:20:14 +0900

    Create Hello judge.cpp

---
+++ Hello judge.cpp
@@ -0,0 +1,28 @@
+無理やりクラスを使って書いた練習
+
+
+#include <iostream>
+
+class sample{
+    public:
+        std::string A;
+        std::string moto();
+};
+
+std::string sample::moto(){
+    std::string B;
+    B = "Hello,World!";
+    return B;
+}
+
+int main(){
+    sample data;
+    std::cin >> data.A;
+    if(data.A == data.moto()){
+        std::cout << "AC" << std::endl;
+    }
+    else{
+        std::cout << "WA" << std::endl;
+    }
+    return 0;
+}
