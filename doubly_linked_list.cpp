7a8de89ff03089f79cd84e4bc1f970449ef08dc3
Kenzo-Fujisaki <85381022+Kenzo-Fujisaki@users.noreply.github.com>
2022-08-14 16:25:32 +0900

    Update doubly_linked_list.cpp

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -7,6 +7,15 @@ typedef struct list{
     struct list *next;
 }list;
 
+//リストが存在するか確かめる関数
+int check_list(list *checker) { 
+    if (checker == nullptr) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 //最後尾までlistをたどる
 list *search_list(list *searcher){
     while(searcher -> next != nullptr){
@@ -17,16 +26,27 @@ list *search_list(list *searcher){
 
 //リストを末尾に追加する
 list *add_list(list *list_head,int data){
-    //最後尾まで辿った後に追加する
+    int check;
     list* receiver = nullptr;
     list* creater = nullptr;
-    receiver = search_list(list_head);//アドレスを辿り最後尾の要素のアドレスを受け取る
-    creater = new list;//動的な領域を確保
-    creater -> data = data;
-    creater -> prev = receiver;
-    creater -> next = nullptr;
-    receiver -> next = creater; //追加したリストの要素と最後尾の要素の連結
-    return list_head;
+    check = check_list(list_head); //リストが存在するかチェック
+    if(check == 1){
+        creater = new list;
+        creater -> data = data;
+        creater -> prev = nullptr;
+        creater -> next = nullptr;
+        return creater;
+    }
+    //最後尾まで辿った後に追加する
+    else{
+        receiver = search_list(list_head);//最後尾までアドレスを辿り最後尾の要素のアドレスを受け取る
+        creater = new list;//動的な領域を確保
+        creater -> data = data;
+        creater -> prev = receiver;
+        creater -> next = nullptr;
+        receiver -> next = creater; //追加したリストの要素と最後尾の要素の連結
+        return list_head;
+    }
 }
 //リストを先頭から最後まで走査して全件表示
 void show_list(list *show){
@@ -34,6 +54,7 @@ void show_list(list *show){
         std::cout << show -> data << ",";
         show = show -> next;
     }
+    std::cout << " " <<std::endl;
 }
 //リストを全件削除する
 void del_list(list *deleter){
@@ -43,6 +64,7 @@ void del_list(list *deleter){
         delete deleter -> prev;
     }
     delete deleter;
+    std::cout << "削除しました" << std::endl;
 }
 //main関数
 int main(){
@@ -53,7 +75,7 @@ int main(){
     //追加
     for(int i=0;i<N;i++){
         int num;
-        std::cout << i+1 <<":";
+        std::cout << i+1 <<"つ目：";
         std::cin >> num;
         main_list = add_list(main_list,num);
     }
@@ -62,9 +84,6 @@ int main(){
     //削除
     del_list(main_list);
 }
-
-
-
 
 
 
