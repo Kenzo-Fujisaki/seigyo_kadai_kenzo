59e5594f22dc718166d0d8a76f4f7ba002a15cee
Kenzo-Fujisaki <85381022+Kenzo-Fujisaki@users.noreply.github.com>
2022-08-25 15:18:12 +0900

    Update doubly_linked_list.cpp

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -7,17 +7,6 @@ typedef struct list{
     struct list *next;
 }list;
 
-bool judge = false;
-//リストが存在するか確かめる関数
-int check_list(list *checker) {
-    if (checker == nullptr) {
-        judge = true;
-        return judge;
-    } else {
-        return 0;
-    }
-}
-
 //最後尾までlistをたどる
 list *search_list(list *searcher){
     while(searcher -> next != nullptr){
@@ -26,30 +15,29 @@ list *search_list(list *searcher){
     return searcher;
 }
 
+//一つ目
+list *init_list(list *list_head,int data){
+    list* first_receiver = nullptr;
+    list* first_creater = nullptr;
+
+    first_creater = new list; //動的な領域を確保
+    first_creater -> data = data;
+    first_creater -> prev = nullptr;
+    first_creater -> next = nullptr;
+    return first_creater;
+}
 //リストを末尾に追加する
 list *add_list(list *list_head,int data){
-    int check;
     list* receiver = nullptr;
     list* creater = nullptr;
-    check = check_list(list_head); //リストが存在するかチェック
-    //一つ目のリスト作成
-    if(check == judge){
-        creater = new list; //動的な領域を確保
-        creater -> data = data;
-        creater -> prev = nullptr;
-        creater -> next = nullptr;
-        return creater;
-    }
-    //最後尾まで辿った後に追加する
-    else{
-        receiver = search_list(list_head);//最後尾までアドレスを辿り最後尾の要素のアドレスを受け取る
-        creater = new list; //動的な領域を確保
-        creater -> data = data;
-        creater -> prev = receiver;
-        creater -> next = nullptr;
-        receiver -> next = creater; //追加したリストの要素と最後尾の要素の連結
-        return list_head;
-    }
+
+    receiver = search_list(list_head);//最後尾までアドレスを辿り最後尾の要素のアドレスを受け取る
+    creater = new list; //動的な領域を確保
+    creater -> data = data;
+    creater -> prev = receiver;
+    creater -> next = nullptr;
+    receiver -> next = creater; //追加したリストの要素と最後尾の要素の連結
+    return list_head;
 }
 //リストを先頭から最後まで走査して全件表示
 void show_list(list *show){
@@ -78,9 +66,16 @@ int main(){
     //追加
     for(int i=0;i<N;i++){
         int num;
-        std::cout << i+1 <<"つ目：";
-        std::cin >> num;
-        main_list = add_list(main_list,num);
+        if(i == 0){
+            std::cout << i+1 <<"つ目：";
+            std::cin >> num;
+            main_list = init_list(main_list,num);
+        }
+        else{
+            std::cout << i+1 <<"つ目：";
+            std::cin >> num;
+            main_list = add_list(main_list,num);
+        }
     }
     //先頭から最後まで走査して出力
     show_list(main_list);
